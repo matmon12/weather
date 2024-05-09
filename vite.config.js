18fdabb1cb620edcf72ce610b1f24382a055c2fc
matmon12 <matveym151@gmail.com>
2024-05-09 15:22:24 +0300

    optimizer

--- vite.config.js
+++ vite.config.js
@@ -11,53 +11,48 @@ import { PrimeVueResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/weather/",
-  envPrefix: "VITE_",   
-  define: {
-    'process.env.VITE_BASE_URL_WEATHER':
-    JSON.stringify('https://api.openweathermap.org/data/2.5/weather'),
-  },
   plugins: [
     vue(),
-    // ViteImageOptimizer({
-    //   svg: {
-    //     multipass: true,
-    //     plugins: [
-    //       {
-    //         name: "preset-default",
-    //         params: {
-    //           overrides: {
-    //             cleanupNumericValues: false,
-    //             removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-    //           },
-    //           cleanupIDs: {
-    //             minify: false,
-    //             remove: false,
-    //           },
-    //           convertPathData: false,
-    //         },
-    //       },
-    //       "sortAttrs",
-    //       {
-    //         name: "addAttributesToSVGElement",
-    //         params: {
-    //           attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-    //         },
-    //       },
-    //     ],
-    //   },
-    //   png: {
-    //     quality: 100,
-    //   },
-    //   jpeg: {
-    //     quality: 100,
-    //   },
-    //   jpg: {
-    //     quality: 100,
-    //   },
-    //   webp: {
-    //     lossless: true,
-    //   },
-    // }),
+    ViteImageOptimizer({
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+              },
+              cleanupIDs: {
+                minify: false,
+                remove: false,
+              },
+              convertPathData: false,
+            },
+          },
+          "sortAttrs",
+          {
+            name: "addAttributesToSVGElement",
+            params: {
+              attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+            },
+          },
+        ],
+      },
+      png: {
+        quality: 100,
+      },
+      jpeg: {
+        quality: 100,
+      },
+      jpg: {
+        quality: 100,
+      },
+      webp: {
+        lossless: true,
+      },
+    }),
     Components({ resolvers: [IconsResolver(), PrimeVueResolver()] }),
     Icons({
       /* options */
