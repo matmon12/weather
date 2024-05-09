c33e3b379d5aa2184d549f4f03bd66be72bfa940
matmon12 <matveym151@gmail.com>
2024-05-09 11:08:46 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -13,46 +13,46 @@ export default defineConfig({
   base: "/weather/",
   plugins: [
     vue(),
-    ViteImageOptimizer({
-      svg: {
-        multipass: true,
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: {
-                cleanupNumericValues: false,
-                removeViewBox: false, // https://github.com/svg/svgo/issues/1128
-              },
-              cleanupIDs: {
-                minify: false,
-                remove: false,
-              },
-              convertPathData: false,
-            },
-          },
-          "sortAttrs",
-          {
-            name: "addAttributesToSVGElement",
-            params: {
-              attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-            },
-          },
-        ],
-      },
-      png: {
-        quality: 100,
-      },
-      jpeg: {
-        quality: 100,
-      },
-      jpg: {
-        quality: 100,
-      },
-      webp: {
-        lossless: true,
-      },
-    }),
+    // ViteImageOptimizer({
+    //   svg: {
+    //     multipass: true,
+    //     plugins: [
+    //       {
+    //         name: "preset-default",
+    //         params: {
+    //           overrides: {
+    //             cleanupNumericValues: false,
+    //             removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+    //           },
+    //           cleanupIDs: {
+    //             minify: false,
+    //             remove: false,
+    //           },
+    //           convertPathData: false,
+    //         },
+    //       },
+    //       "sortAttrs",
+    //       {
+    //         name: "addAttributesToSVGElement",
+    //         params: {
+    //           attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+    //         },
+    //       },
+    //     ],
+    //   },
+    //   png: {
+    //     quality: 100,
+    //   },
+    //   jpeg: {
+    //     quality: 100,
+    //   },
+    //   jpg: {
+    //     quality: 100,
+    //   },
+    //   webp: {
+    //     lossless: true,
+    //   },
+    // }),
     Components({ resolvers: [IconsResolver(), PrimeVueResolver()] }),
     Icons({
       /* options */
